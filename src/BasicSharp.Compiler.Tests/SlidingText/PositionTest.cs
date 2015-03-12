7c179c3e73a49d9033ad350c83a01b91462e01ee
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2015-03-12 08:47:55 -0300

    Adicionado nova unidade de testes
    Criado unidade de testes para posição de SlidingTextSource

---
+++ src/BasicSharp.Compiler.Tests/SlidingText/PositionTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace BasicSharp.Compiler.Tests.SlidingText
+{
+    [TestClass]
+    public class PositionTest
+    {
+        [TestMethod]
+        public void Position_WhenJustPeeking_ShouldBeZero()
+        {
+            var source = "123.123.123";
+            var text = SlidingTextSources.GetSlidingTextWith(source);
+
+            for (int i = 0; i < source.Length; i++)
+                text.Peek(i);
+
+            text.Position.Should().Be(0, "because Next has never been called");
+        }
+
+        [TestMethod]
+        public void Position_WhenCallingNext_ShouldBeCorrect()
+        {
+            var source = "123.123.123";
+            var text = SlidingTextSources.GetSlidingTextWith(source);
+
+            text.Next(5);
+
+            text.Position.Should().Be(5, "because Next has been called by 5 times");
+        }
+    }
+}
