76a293101a92d568adbefc0746fa3df971b3ff6b
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2015-03-22 22:11:56 -0300

    Progresso no desenvolvimento do Parser
    Arquivos não presentes no último commit

---
+++ src/BasicSharp.Compiler/Parser/Syntaxes/SyntaxNode.cs
@@ -0,0 +1,25 @@
+﻿using BasicSharp.Compiler.Lexer;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicSharp.Compiler.Parser.Syntaxes
+{
+    public abstract class SyntaxNode
+    {
+        List<TokenInfo> trivias = new List<TokenInfo>();
+
+        public ReadOnlyCollection<TokenInfo> Trivias
+        {
+            get { return new ReadOnlyCollection<TokenInfo>(trivias); }
+        }
+
+        public void AddTrivia(TokenInfo trivia)
+        {
+            trivias.Add(trivia);
+        }
+    }
+}
