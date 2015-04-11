21805956ba9f49fc72ee012f27b1af8f01ff5fa5
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2015-04-11 09:21:22 -0300

    Arquivo que não foi no último commit

---
+++ src/BasicSharp.IDE/ViewModel/ProjectViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicSharp.IDE.ViewModel
+{
+    public class ProjectViewModel : ViewModelBase 
+    {
+        public ObservableCollection<string> AssembliesAddress { get; set; }
+        public string SourceAddress { get; set; }
+        public string Name { get; set; }
+    }
+}
